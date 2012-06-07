525b673451eda9470105fd23c045d1b4bffe3cbc
jy02140486@gmail.com <jy02140486@gmail.com@9e396f09-0e8d-d9d0-56cf-50f984d1cd9c>
2012-06-07 12:40:07 +0000

    Improve the guide texts

--- trunk/Engine/render.cpp
+++ trunk/Engine/render.cpp
@@ -6,18 +6,21 @@ void T_App::render()
 {
 	mpDisplayWindow->get_gc().clear(CL_Colorf::black);
 	
-	words->draw_text(mpDisplayWindow->get_gc(),40,20,"Press A/D to act, Space to reset, Esc to Quit",CL_Colorf::yellow);
-	words->draw_text(mpDisplayWindow->get_gc(),40,_datapool.screenH-30,"Deliever the ball back to Billy",CL_Colorf::yellow);
+	
+	
 
 	switch (global_state)
 	{
 	case COMMON:
-
 		_datapool.drawall();
 		_datapool.drawBilly(&(mpDisplayWindow->get_gc()),_datapool.Billy);
+		words->draw_text(mpDisplayWindow->get_gc(),40,_datapool.screenH-30,"Deliever the ball back to Billy",CL_Colorf::yellow);
+		words->draw_text(mpDisplayWindow->get_gc(),40,20,"Press A/D to act, Space to reset, Esc to Quit",CL_Colorf::yellow);
 		break;
 	case ALL_CLEAR:
-		_datapool.AllClear->draw(mpDisplayWindow->get_gc(),10,400);
+		words->draw_text(mpDisplayWindow->get_gc(),40,_datapool.screenH-30,"Good job!",CL_Colorf::yellow);
+		words->draw_text(mpDisplayWindow->get_gc(),40,20,"Press Space to restart, Esc to Quit",CL_Colorf::yellow);
+		_datapool.AllClear->draw(mpDisplayWindow->get_gc(),10,_datapool.screenH/3);
 		break;
 	default:;
 	}
